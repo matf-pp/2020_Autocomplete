da0ea6831a52f6591a1d28a944c7cfb08fe34e39
vojann <jovan.ran87@gmail.com>
2020-04-25 23:48:14 +0200

    zapoceo gui

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+from tkinter import *
 
 def lexicographic_sort(lista):
     lista.sort(key=lambda x:x[0])
@@ -10,7 +10,7 @@ def weight_sort(lista):
     return lista
 
 def sort_first_r(lista,r):
-    lista.sort(key=lambda x:x[0][0:len(r)])
+    lista.sort(key=lambda x:x[0][0:r])
 
 def binarna_pretraga(uzorak,lista):
     l=0
@@ -43,3 +43,20 @@ for linija in f:
             text[1]+=" "+text[i]
     torka=(text[1],text[0])
     lista.append(torka)
+try:
+    n1=sys.argv[2]
+    br=int(n1)
+except(IndexError):
+    exit("Navedite broj")
+
+
+
+
+
+root=Tk()
+
+root.title("Autocomplete me")
+root.geometry("500x250")
+l1=Label(root,text="Search query:",bg="white",fg="black")
+l1.place(x=35,y=1)
+root.mainloop()
